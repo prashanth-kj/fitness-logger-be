a5e453874b2db8f38c60ab66346d1613f6be9827
prashanth-kj <kjprasanth98@gmail.com>
2023-11-23 19:35:26 +0530

    comments added

--- src/routes/exerciseRoutes.js
+++ src/routes/exerciseRoutes.js
@@ -4,12 +4,13 @@ import resistanceController from '../controller/resistanceController.js';
 import Auth from '../common/Auth.js';
 const router= express.Router();
 
+//cardio routes
 router.post('/cardio', Auth.validate, cardioController.createCardio);
 router.get('/cardio',Auth.validate,cardioController.getCardioByUserId);
 router.get('/cardio/:id',Auth.validate,cardioController.getCardioById);
 router.delete('/cardio/:id',cardioController.deleteCardioById);
 
-
+//Resistance routes
 router.post('/resistance',Auth.validate,resistanceController.createResistance);
 router.get('/resistance',Auth.validate,resistanceController.getResistanceByUserId);
 router.get('/resistance/:id',Auth.validate,resistanceController.getResistancebyId);
